aba170c903b784fbb18c49d223b6fff05d822ee9
Mark Sun <sunmark14@gmail.com>
2021-01-09 10:26:05 -0500

    Additional changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default () => {
+  const [pokemonName, setPokemonName] = useState("pikachu");
+  const [pokemonImgUrl, setPokemonImgUrl] = useState(
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+  );
+  const [debouncedPokemonName, setDebouncedPokemonName] = useState(pokemonName);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedPokemonName(pokemonName);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [pokemonName]);
+
+  useEffect(() => {
+    const search = async () => {
+      if (debouncedPokemonName) {
+        setPokemonImgUrl("");
+        try {
+          setHasError(false);
+          const { data } = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${debouncedPokemonName}/`
+          );
+          setPokemonImgUrl(data.sprites.front_default);
+        } catch (e) {
+          setHasError(true);
+        }
+      }
+    };
+    search();
+  }, [debouncedPokemonName]);
+
+  const onInputChange = (event) => {
+    setPokemonName(event.target.value);
+  };
+
+  const renderResult = () => {
+    if (!hasError && pokemonImgUrl) {
+      return (
+        <div>
+          <img src={pokemonImgUrl} alt={debouncedPokemonName}></img>
+        </div>
+      );
+    } else if (!hasError && pokemonImgUrl === "") {
+      return <div>Please wait</div>;
+    } else if (hasError) {
+      return <div>Whoops, something went wrong. please try again!</div>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="name-input">Search for Pokemon</label>
+      <input id="name-input" onChange={onInputChange} value={pokemonName} />
+      {renderResult()}
     </div>
   );
-}
-
-export default App;
+};
